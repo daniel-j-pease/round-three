30b7324f26860d315a7d3def9d448390ea9ac105
Daniel Pease <daniel.j.pease@gmail.com>
2017-01-17 17:22:01 -0500

    add media query

--- public/script.js
+++ public/script.js
@@ -3,16 +3,25 @@ console.log('script.js connected');
 
 window.onload = () => {
 
+  const viewHeight = window.innerHeight;
   let hamburger = document.getElementById('hamburger')
   let nav = document.querySelector('nav');
   let body = document.querySelector('body');
+  let first = document.querySelector('#first');
+  let last = document.querySelector('#last');
+  let barOne = document.querySelector('#bar-one');
+  let barTwo = document.querySelector('#bar-two');
+  let barThree = document.querySelector('#bar-three');
+  let posFirst = document.querySelector('#positioner-first');
+  let posLast = document.querySelector('#positioner-last');
+  let title = document.querySelector('#title');
 
   function handleHamburgerClick() {
     nav.style.display = 'block';
-    nav.className = 'trans';
   }
 
   function hideNav(e) {
+    console.log(e.target)
     if (e.target !== hamburger && e.srcElement.nodeName !== "SPAN") {
       nav.style.display = 'none';
     }
@@ -21,7 +30,6 @@ window.onload = () => {
   function handleNavScroll(e) {
 
     // define scroll position of each section relative to window.innerHeight
-    const viewHeight = window.innerHeight;
     const homeHeight = 0;
     const aboutHeight = viewHeight;
     const projectsHeight = viewHeight * 2;
@@ -52,8 +60,50 @@ window.onload = () => {
     }, 10)
   }
 
+  function checkHamburger() {
+    console.log(viewHeight)
+    if (document.body.scrollTop > (viewHeight * 2)) {
+      console.log(hamburger.childNodes)
+      hamburger.childNodes[1].style.backgroundColor = '#355C7D';
+      hamburger.childNodes[3].style.backgroundColor = '#355C7D';
+      hamburger.childNodes[5].style.backgroundColor = '#355C7D';
+    } else {
+      hamburger.childNodes[1].style.backgroundColor = '';
+      hamburger.childNodes[3].style.backgroundColor = '';
+      hamburger.childNodes[5].style.backgroundColor = '';
+    }
+  }
+
+  function moveBars() {
+    console.log('moving')
+    let firstBarTop = first.getBoundingClientRect().top + (first.getBoundingClientRect().bottom - first.getBoundingClientRect().top)/3;
+    let firstBarLeft = last.getBoundingClientRect().left;
+    let lastBarTop = last.getBoundingClientRect().top + (last.getBoundingClientRect().bottom - last.getBoundingClientRect().top)/2;
+
+    let posFirstRight = posFirst.getBoundingClientRect().right;
+    let posFirstLeft = posFirst.getBoundingClientRect().left;
+    let posLastRight = posLast.getBoundingClientRect().right;
+
+    barOne.style.top = `${firstBarTop}px`;
+    barOne.style.left = `${firstBarLeft + 10}px`;
+    barOne.style.width = `${posFirstLeft-firstBarLeft - 15}px`
+
+    barTwo.style.top = `${lastBarTop}px`;
+    barTwo.style.width = `${posFirstRight - posLastRight - 20}px`
+    barTwo.style.left = `${posFirstRight - barTwo.offsetWidth - 10}px`
+
+    barThree.style.top = `${title.getBoundingClientRect().bottom + 5}px`;
+    barThree.style.width = `${(title.offsetWidth/2)}px`;
+    barThree.style.height = `.3vh`;
+    barThree.style.left = `${(window.innerWidth - barThree.offsetWidth)/2}px`;
+  }
+
   // add event listeners
   hamburger.addEventListener('click', handleHamburgerClick);
   body.addEventListener('click', hideNav);
   nav.addEventListener('click', handleNavScroll);
+  window.addEventListener('resize', moveBars)
+  window.addEventListener('scroll', checkHamburger);
+
+  moveBars();
 };
